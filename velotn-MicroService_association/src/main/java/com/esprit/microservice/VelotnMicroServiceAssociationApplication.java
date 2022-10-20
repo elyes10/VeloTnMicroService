936672978ba70a690deffbccd41a75092061ad0f
Yassine.Makhlouf <yassine.makhlouf@esprit.tn>
2022-10-20 01:08:44 +0100

    config euroka

--- velotn-MicroService_association/src/main/java/com/esprit/microservice/VelotnMicroServiceAssociationApplication.java
+++ velotn-MicroService_association/src/main/java/com/esprit/microservice/VelotnMicroServiceAssociationApplication.java
@@ -2,8 +2,10 @@ package com.esprit.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VelotnMicroServiceAssociationApplication {
 
 	public static void main(String[] args) {
