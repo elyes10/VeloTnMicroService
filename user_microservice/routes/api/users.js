3aab4df908ee3b02ae3c0cccb4dcb8dcd86a1950
elyes10 <47626987+elyes10@users.noreply.github.com>
2022-10-24 10:37:44 +0100

    nodeJs service
    login + user management
    working with docker

---
+++ user_microservice/routes/api/users.js
@@ -0,0 +1,139 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require("passport");
+
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+const render= require("xlsx")
+// Load User model
+const User = require("../../models/User");
+
+// @route POST api/users/register
+// @desc Register user
+// @access Public
+router.post("/register", (req, res) => {
+  // Form validation
+
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      // Hash password before saving in database
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+// @route POST api/users/login
+// @desc Login user and return JWT token
+// @access Public
+router.post("/login", (req, res) => {
+  // Form validation
+
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check if user exists
+    if (!user) {
+      return res.status(404).json({ emailnotfound: "Email not found" });
+    }
+
+    // Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User matched
+        // Create JWT Payload
+        const payload = {
+          id: user.id,
+          name: user.name
+        };
+
+        // Sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          {
+            expiresIn: 31556926 // 1 year in seconds
+          },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        return res
+          .status(400)
+          .json({ passwordincorrect: "Password incorrect" });
+      }
+    });
+  });
+});
+
+/* show all additions. http://localhost:5000/users/show */
+router.get('/show',function (req,res) {
+  User.find({}, (err, additions) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err })
+    }
+
+    return res.status(200).json(additions)
+  }).catch(err => console.log(err))
+})
+
+/* delete additions. http://localhost:3000/api/users/delete/id */
+router.delete('/delete/:id',function (req,res) {
+  User.findOneAndDelete({ _id: req.params.id }, (err, additions) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err })
+    }
+
+    if (!additions) {
+      return res
+          .status(404)
+          .json({ success: false, error: `user not found` })
+    }
+
+    return res.status(200).json({ success: true, data: additions })
+  }).catch(err => console.log(err))
+})
+
+
+
+module.exports = router;
